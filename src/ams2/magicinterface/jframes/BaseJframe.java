65aa4d8b38be894b6959818f2f9189f71ffa3633
JCapelDev <57891345+JCapelDev@users.noreply.github.com>
2020-11-08 21:50:43 +0100

    Commit Same Day advanced

--- src/ams2/magicinterface/jframes/BaseJframe.java
+++ src/ams2/magicinterface/jframes/BaseJframe.java
@@ -6,6 +6,12 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.SwingConstants;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class BaseJframe extends JFrame {
 
@@ -14,29 +20,68 @@ public class BaseJframe extends JFrame {
 	/**
 	 * Launch the application.
 	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					BaseJframe frame = new BaseJframe();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+//	public static void main(String[] args) {
+//		EventQueue.invokeLater(new Runnable() {
+//			public void run() {
+//				try {
+//					BaseJframe frame = new BaseJframe();
+//					frame.setVisible(true);
+//				} catch (Exception e) {
+//					e.printStackTrace();
+//				}
+//			}
+//		});
+//	}
 
 	/**
 	 * Create the frame.
 	 */
 	public BaseJframe() {
+		setVisible(true);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 547, 377);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblTittle = new JLabel("Gestor de Cartas de Magic the Gathering\u2122");
+		lblTittle.setHorizontalAlignment(SwingConstants.CENTER);
+		lblTittle.setFont(new Font("Tahoma", Font.BOLD, 20));
+		lblTittle.setBounds(47, 11, 433, 62);
+		contentPane.add(lblTittle);
+		
+		JButton btnAddCard = new JButton("A\u00F1adir Carta");
+		btnAddCard.setBounds(36, 252, 110, 32);
+		contentPane.add(btnAddCard);
+		
+		btnAddCard.addActionListener(new ActionListener(){
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				new AddCardJframe();
+				dispose();
+			}
+			
+		});
+		
+		JButton btnViewCards = new JButton("Ver Cartas");
+		btnViewCards.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnViewCards.setBounds(199, 252, 102, 32);
+		contentPane.add(btnViewCards);
+		
+		JButton btnDeleteCard = new JButton("Eliminar Carta");
+		btnDeleteCard.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnDeleteCard.setBounds(353, 252, 132, 32);
+		contentPane.add(btnDeleteCard);
 	}
 
+	
 }
--- src/ams2/magicinterface/test/MagicInterfaceLauncher.java
+++ src/ams2/magicinterface/test/MagicInterfaceLauncher.java
@@ -1,9 +1,12 @@
 package ams2.magicinterface.test;
 
+import ams2.magicinterface.jframes.BaseJframe;
+
 public class MagicInterfaceLauncher {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		new BaseJframe();
 
 	}
 
